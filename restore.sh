7292c0f6a64839bdc2bfdeddfc505f6bb3b18408
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-20 08:30:38 +0800

    restart container

--- restore.sh
+++ restore.sh
@@ -11,3 +11,6 @@ docker stop baserow_vocabai
 
 echo "restoring"
 docker run --rm -v baserow_data:/baserow/data -v $BACKUP_DIR:/backup ubuntu bash -c "rm -rvf /baserow/data/* && mkdir -p /baserow/data && cd /baserow/data && tar xvf /backup/${BACKUP_FILENAME} --strip-components=2"
+
+echo "restarting container"
+docker start baserow_vocabai

e319c517fc0e6891feb9867b82a4ca38826e2d98
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-20 08:51:42 +0800

    upload backup to wasabi

--- backup.sh
+++ backup.sh
@@ -4,7 +4,14 @@ BACKUP_DIR=$HOME/backup
 BACKUP_FILENAME=baserow_vocabai_`hostname`.tar.gz
 mkdir -p ${BACKUP_DIR}
 
+echo "stopping container"
 docker stop baserow_vocabai
+
+echo "running backup"
 docker run --rm -v baserow_data:/baserow/data -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/${BACKUP_FILENAME} /baserow/data
 
+echo "restarting container"
 docker start baserow_vocabai
+
+echo "running rclone"
+rclone sync -v $BACKUP_DIR/$BACKUP_FILENAME baserow-vocabai-backup:baserow-vocabai-backup

456703a9c90f43fe0577285babc68c5af082a866
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-27 21:50:21 +0800

    set stats url

--- run.sh
+++ run.sh
@@ -3,6 +3,20 @@
 HOSTNAME=`hostname`
 . /root/baserow_vocabai_settings.sh
 
+if [[ -z "${CLOUDLANGUAGETOOLS_CORE_KEY}" ]]; then
+  echo "CLOUDLANGUAGETOOLS_CORE_KEY not set"
+  exit 1
+fi
+
+if [[ -z "${BASEROW_USER_STATS_URL}" ]]; then
+  echo "BASEROW_USER_STATS_URL not set"
+  exit 1
+fi
+
+if [[ -z "${BASEROW_USER_STATS_TOKEN}" ]]; then
+  echo "BASEROW_USER_STATS_TOKEN not set"
+  exit 1
+fi
 
 docker run \
   -d \
@@ -12,6 +26,8 @@ docker run \
   -e CLOUDLANGUAGETOOLS_CORE_KEY=${CLOUDLANGUAGETOOLS_CORE_KEY} \
   -e BASEROW_TRIGGER_SYNC_TEMPLATES_AFTER_MIGRATION=false \
   -e BASEROW_CELERY_CLT_WORKER_NUM=3 \
+  -e BASEROW_USER_STATS_URL=${BASEROW_USER_STATS_URL} \
+  -e BASEROW_USER_STATS_TOKEN=${BASEROW_USER_STATS_TOKEN} \
   -v baserow_data:/baserow/data \
   -p 80:80 \
   -p 443:443 \
